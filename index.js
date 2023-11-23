314422d7e62f2d9ef8a835be4e51ae405124b4af
Shariar Islam <shariarn85@gmail.com>
2023-11-24 02:42:46 +0600

    inital packages installed and used in file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+const port = process.env.PORT || 5000
+
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
