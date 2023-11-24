2897ce721cc2b0a248d4da5b5048b86344471938
Shariar Islam <shariarn85@gmail.com>
2023-11-25 00:57:52 +0600

    verification api implemented

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 // middleware
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:5174'],
-  credentials: true,
+  // credentials: true,
   optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
@@ -32,6 +32,7 @@ const client = new MongoClient(uri, {
   }
 });
 
+const userCollection = client.db('SoulMateConnectDB').collection('users')
 const biodatasCollection = client.db('SoulMateConnectDB').collection('biodatas')
 
 
@@ -41,6 +42,56 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+     // jwt related api 
+     
+        // jwt related api 
+        app.post('/jwt', async (req, res) => {
+          const user = req.body;
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
+          res.send({ token })
+      })
+
+      // middlewares 
+      const verifyToken = (req, res, next) => {
+          // console.log('inside verify token', req.headers.authorization);
+          if (!req.headers.authorization) {
+              return res.status(401).send({ message: 'unauthorized access' })
+          }
+          const token = req.headers.authorization.split(' ')[1]
+          jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+              if (err) {
+                  return res.status(401).send({ message: 'unauthorized access' })
+              }
+              req.decoded = decoded;
+              next()
+          })
+      }
+
+      // verify admin  (have to verify after verify token)
+      const verifyAdmin = async (req, res, next) => {
+          const email = req.decoded.email;
+          const query = { email: email }
+          const user = await userCollection.findOne(query)
+          const isAdmin = user?.role === 'admin'
+          if (!isAdmin) {
+              return res.send(403).send({ message: 'forbidden access' })
+          }
+          next()
+      }
+
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+
+        // insert email if user doesn't exists 
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query)
+        if (existingUser) {
+            return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+    
 
 
     app.get('/biodatas', async (req, res) => {
@@ -48,7 +99,7 @@ async function run() {
       res.send(result)
 
     })
-    app.get('/biodata-details/:id', async (req, res) => {
+    app.get('/biodata-details/:id', verifyToken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await biodatasCollection.findOne(query)
